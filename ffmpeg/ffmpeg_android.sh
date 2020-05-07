f0e860ca5034b3e9fc67d70ba1de2605b9f2e506
liuxin.fish.one <liuxin.fish.one@bytedance.com>
2020-05-08 07:44:14 +0800

    Feature: ffmpeg build script

---
+++ ffmpeg/ffmpeg_android.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+export TMPDIR=/tmp/ffmpeg_build
+if [[ ! -d ${TMPDIR} ]]; then
+    mkdir -p ${TMPDIR}
+fi
+if [[ -d $(pwd)/android ]]; then
+    rm -r $(pwd)/android
+fi
+echo $(uname)
+if [[ $(uname) == "Darwin" ]]; then
+    export HOST=darwin-x86_64
+elif [[ $(uname) == "Linux" ]]; then
+    export HOST=linux-x86_64
+else
+    echo "Unsupported platform:$(uname)"
+    exit 1
+fi
+
+export NDK=${ANDROID_HOME}/ndk/20.1.5948944
+TOOLCHAIN=${NDK}/toolchains/llvm/prebuilt/${HOST}/
+SYSROOT=${TOOLCHAIN}/sysroot
+API=29
+
+function build_ffmpeg() {
+    echo "Compiling FFmpeg for ${CPU}"
+
+    ./configure \
+        --prefix=${PREFIX} \
+        --disable-programs \
+        --disable-doc \
+        --disable-static \
+        --disable-symver \
+        --disable-postproc \
+        --disable-avdevice \
+        --enable-pic \
+        --enable-asm \
+        --enable-neon \
+        --enable-shared \
+        --enable-hwaccels \
+        --enable-gpl \
+        --enable-jni \
+        --enable-mediacodec \
+        --enable-decoder=h264_mediacodec \
+        --cross-prefix=${CROSS_PREFIX} \
+        --target-os=android \
+        --arch=${ARCH} \
+        --cpu=${CPU} \
+        --cc=${CC} \
+        --cxx=${CXX} \
+        --enable-cross-compile \
+        --sysroot=${SYSROOT} \
+        --extra-cflags="-Os -fPIC ${OPTIMIZE_CFLAGS}" \
+        --extra-ldflags="$OPTIMIZE_CFLAGS" \
+        ${ADDITIONAL_CONFIGURE_FLAG}
+    make clean
+    make -j8
+    make install
+
+    echo "The Compilation of FFmpeg for ${CPU} is completed"
+}
+
+#armv8-a
+ARCH=arm64
+CPU=armv8-a
+CC=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang++
+CROSS_PREFIX=${TOOLCHAIN}/bin/aarch64-linux-android-
+PREFIX=$(pwd)/android/${CPU}
+OPTIMIZE_CFLAGS="-march=${CPU} -DANDROID -D__ARMEL__"
+build_ffmpeg
+
+#armv7-a
+ARCH=arm
+CPU=armv7-a
+CC=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang
+CXX=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang++
+CROSS_PREFIX=${TOOLCHAIN}/bin/arm-linux-androideabi-
+PREFIX=$(pwd)/android/${CPU}
+OPTIMIZE_CFLAGS="-march=${CPU} -DANDROID -D__ARMEL__ -mfloat-abi=softfp -mfpu=neon -marm"
+build_ffmpeg
+
+#x86
+ARCH=x86
+CPU=x86
+CC=${TOOLCHAIN}/bin/i686-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/i686-linux-android${API}-clang++
+CROSS_PREFIX=${TOOLCHAIN}/bin/i686-linux-android-
+PREFIX=$(pwd)/android/${CPU}
+OPTIMIZE_CFLAGS="-march=i686 -DANDROID -mssse3 -m32 -mtune=intel -mfpmath=sse"
+ADDITIONAL_CONFIGURE_FLAG="--disable-asm"
+build_ffmpeg
+
+#x86_64
+ARCH=x86_64
+CPU=x86-64
+CC=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang++
+CROSS_PREFIX=${TOOLCHAIN}/bin/x86_64-linux-android-
+PREFIX=$(pwd)/android/${CPU}
+OPTIMIZE_CFLAGS="-march=${CPU} -DANDROID -msse4.2 -m64 -mtune=intel -mpopcnt"
+ADDITIONAL_CONFIGURE_FLAG=""
+build_ffmpeg
