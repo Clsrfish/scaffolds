bef885995b532a89aeb1c134a80071a292d9eccb
liuxin.fish.one <liuxin.fish.one@bytedance.com>
2020-09-16 10:38:55 +0800

    add git_tags

--- zsh_plugins/plugin_git.zsh
+++ zsh_plugins/plugin_git.zsh
@@ -1,3 +1,7 @@
 function gitopen() {
     git remote get-url origin | awk -F '[@:]' '{print "http://" $2 "/" $3}' | xargs open
 }
+
+function git_tags() {
+  git log --date-order --graph --tags --simplify-by-decoration --pretty=format:'%ai %h %d'
+}
