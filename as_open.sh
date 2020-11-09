383139830060d46bb50b846d7e9efc9cfb3b6c58
liuxin.fish.one <liuxin.fish.one@bytedance.com>
2020-11-09 13:48:57 +0800

    update asopen

--- as_open.sh
+++ as_open.sh
@@ -2,8 +2,6 @@
 
 AS_DIR=${HOME}/ByteDance/Projects
 
-alias ANDROID_STUDIO="open -a /Applications/Android\ Studio.app"
-
 select proj in $(ls -Al ${AS_DIR} | grep '^d' | awk '{print $9}') "Quit"; do
     if [[ ${proj} == "Quit" ]]; then
         echo "Quit."
@@ -11,10 +9,8 @@ select proj in $(ls -Al ${AS_DIR} | grep '^d' | awk '{print $9}') "Quit"; do
     elif [[ -z ${proj} ]]; then
         echo "Invalid project:${proj}, please try again."
     else
-        ANDROID_STUDIO ${AS_DIR}/${proj}
+        androidstudio ${AS_DIR}/${proj}
         cd ${AS_DIR}/${proj}
         break
     fi
 done
-
-unalias ANDROID_STUDIO
